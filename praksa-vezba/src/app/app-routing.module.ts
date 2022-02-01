5ad6d0b1413e60da3b2f57f292daf3f11e020e88
Dejan Culic <dejan933.culic@gmail.com>
2022-02-01 11:42:18 +0100

    Tab 5 i 6

--- praksa-vezba/src/app/app-routing.module.ts
+++ praksa-vezba/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { Tab1Component } from './tab1/tab1.component';
-import { Tab2Component } from './tab2/tab2.component';
+import { Tab1Component } from './component/tab1/tab1.component';
+import { Tab2Component } from './component/tab2/tab2.component';
 import { Tab3Component } from './component/tab3/tab3.component';
 import { Tab4Component } from './component/tab4/tab4.component';
+import { Tab5Component } from './component/tab5/tab5.component';
+import { Tab6Component } from './component/tab6/tab6.component';
 
 
 const routes: Routes = [
@@ -11,6 +13,8 @@ const routes: Routes = [
   { path: 'tab2', component: Tab2Component},
   { path: 'tab-3', component: Tab3Component },
   { path: 'tab-4', component: Tab4Component },
+  { path: 'tab-5', component: Tab5Component },
+  { path: 'tab-6', component: Tab6Component }
 ];
 
 @NgModule({
--- praksa-vezba/src/app/app.module.ts
+++ praksa-vezba/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Tab1Component } from './tab1/tab1.component';
-import { Tab2Component } from './tab2/tab2.component';
+import { Tab1Component } from './component/tab1/tab1.component';
+import { Tab2Component } from './component/tab2/tab2.component';
 import { Tab3Component } from './component/tab3/tab3.component';
 import { Tab4Component } from './component/tab4/tab4.component';
+import { Tab5Component } from './component/tab5/tab5.component';
+import { Tab6Component } from './component/tab6/tab6.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { Tab4Component } from './component/tab4/tab4.component';
     Tab1Component,
     Tab2Component,
     Tab3Component,
-     Tab4Component
+     Tab4Component,
+     Tab5Component,
+     Tab6Component
   ],
   imports: [
     BrowserModule,
