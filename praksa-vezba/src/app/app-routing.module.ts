ecf6e5933d315d4d6dbfedf54987b42932dd8b58
bobanNikolic96 <boban96@gmail.com>
2022-02-01 09:20:08 +0100

    dodate rute i koponente tab1 i tab2

--- praksa-vezba/src/app/app-routing.module.ts
+++ praksa-vezba/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Tab1Component } from './tab1/tab1.component';
+import { Tab2Component } from './tab2/tab2.component';
 
 const routes: Routes = [
-  
+    { path: 'tab1', component: Tab1Component},
+    { path: 'tab2', component: Tab2Component}
 ];
 
 @NgModule({
--- praksa-vezba/src/app/app.module.ts
+++ praksa-vezba/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Tab1Component } from './tab1/tab1.component';
+import { Tab2Component } from './tab2/tab2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Tab1Component,
+    Tab2Component
   ],
   imports: [
     BrowserModule,
