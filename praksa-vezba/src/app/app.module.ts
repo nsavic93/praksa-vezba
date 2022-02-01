87020f02f248aac2df1b1f3c6590ca42bdacd651
Nemanja Savic <nemanja.savic.031@gmail.com>
2022-02-01 10:26:18 +0100

    odradio tab1 i tab 2

--- praksa-vezba/src/app/app.module.ts
+++ praksa-vezba/src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Tab1Component } from './component/tab1/tab1.component';
+import { Tab2Component } from './component/tab2/tab2.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, Tab1Component, Tab2Component],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
